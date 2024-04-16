54ada2e9c1fb020b38b07c2a7ef818513cb0471e
math07mpo <127318836+math07mpo@users.noreply.github.com>
2024-04-16 07:36:12 -0300

    .

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const botoes = document.querySelector(".botao")
 
-
+console.log("teste")
 for(let i=0;i<botoes.length;i++){
 
    botoes[i].onclick=function(){
    botoes[i].classList.add('ativo')
-      console.log("teste")
+      
    }
 }
