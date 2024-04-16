2a4e09c74eae2b48b1493f8c4655f3b7725c24b3
math07mpo <127318836+math07mpo@users.noreply.github.com>
2024-04-16 07:41:47 -0300

    ss

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ console.log("teste")
 for(let i=0;i<botoes.length;i++){
 
    botoes[i].onclick=function(){
+      for(let j=0;j>botoes.length;j++){
+     botoes[i].classList.remove("ativo")
+
+      }
    botoes[i].classList.add('ativo')
       
    }
