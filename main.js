583cfdc9898a4d85be74e8f1b218fc93a5ba66e4
math07mpo <127318836+math07mpo@users.noreply.github.com>
2024-04-16 07:43:29 -0300

    .

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const botoes = document.querySelector(".botao")
 
-console.log("teste")
 for(let i=0;i<botoes.length;i++){
+   console.log(i)
 
    botoes[i].onclick=function(){
       for(let j=0;j>botoes.length;j++){
